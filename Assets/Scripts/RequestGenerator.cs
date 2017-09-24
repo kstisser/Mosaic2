ac614e33c9535374f1e6e59378d9d14a406c83e0
kstisser <fm8733@gmail.com>
2017-09-24 13:00:56 -0400

    Adding last

--- Assets/Scripts/RequestGenerator.cs
+++ Assets/Scripts/RequestGenerator.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace Mosaic {
-    public class RequestGenerator {
+namespace Mosaic
+{
+    public class RequestGenerator:MonoBehaviour
+    {
+        public Text text;
+        HelpRequest request;
+
+        void Start()
+        {
+            request = generateRandomRequest();
+            Debug.Log(request.ToString());
+        }
 
         public HelpRequest generateRandomRequest()
         {
@@ -18,26 +29,26 @@ namespace Mosaic {
             int ppl = UnityEngine.Random.Range(1, 5);
             return (new HelpRequest(latlong, severity, type, ppl, System.DateTime.Now, pickDescriptor(type)));
         }
-        
+
         private string pickDescriptor(HelpRequest.ReliefType type)
         {
-            if(type == HelpRequest.ReliefType.Medicine)
+            if (type == HelpRequest.ReliefType.Medicine)
             {
                 return HelpRequest.MedicalDescription[UnityEngine.Random.Range(0, HelpRequest.MedicalDescription.Length)];
             }
-            else if(type == HelpRequest.ReliefType.Food)
+            else if (type == HelpRequest.ReliefType.Food)
             {
                 return HelpRequest.FoodDescription[UnityEngine.Random.Range(0, HelpRequest.FoodDescription.Length)];
             }
-            else if(type == HelpRequest.ReliefType.Water)
+            else if (type == HelpRequest.ReliefType.Water)
             {
                 return HelpRequest.WaterDescription[UnityEngine.Random.Range(0, HelpRequest.WaterDescription.Length)];
             }
-            else if(type == HelpRequest.ReliefType.Security)
+            else if (type == HelpRequest.ReliefType.Security)
             {
                 return HelpRequest.SecurityDescription[UnityEngine.Random.Range(0, HelpRequest.SecurityDescription.Length)];
             }
-            else if(type == HelpRequest.ReliefType.Evacuation)
+            else if (type == HelpRequest.ReliefType.Evacuation)
             {
                 return HelpRequest.EvacuationDescription[UnityEngine.Random.Range(0, HelpRequest.EvacuationDescription.Length)];
             }
